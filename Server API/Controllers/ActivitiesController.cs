ac247b16f71c8f14a962e0f86a5eee542dc4c5ef
Tom Postler <tom@postler.me>
2015-02-17 23:37:45 -0600

    Somehow got the activities to work. Only GET tested. !!!Passwords in plaintext!!!

--- Server API/Controllers/ActivitiesController.cs
+++ Server API/Controllers/ActivitiesController.cs
@@ -1,35 +1,134 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
+using Server_API.Models;
 
 namespace Server_API.Controllers
 {
-    public class ActivitiesController : ApiController
+    public class activitiesController : ApiController
     {
-        // GET: api/Activities
-        public IEnumerable<string> Get()
+        private csci4950s15Entities db = new csci4950s15Entities();
+
+        // GET: api/activities
+        public IQueryable<activity> Getactivities()
         {
-            return new string[] { "value1", "value2" };
+            return db.activities;
         }
 
-        // GET: api/Activities/5
-        public string Get(int id)
+        // GET: api/activities/5
+        [ResponseType(typeof(activity))]
+        public async Task<IHttpActionResult> Getactivity(int id)
         {
-            return "value";
+            activity activity = await db.activities.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(activity);
         }
 
-        // POST: api/Activities
-        public void Post([FromBody]string value)
+        // PUT: api/activities/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putactivity(int id, activity activity)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != activity.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(activity).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!activityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // PUT: api/Activities/5
-        public void Put(int id, [FromBody]string value)
+        // POST: api/activities
+        [ResponseType(typeof(activity))]
+        public async Task<IHttpActionResult> Postactivity(activity activity)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.activities.Add(activity);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (activityExists(activity.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = activity.id }, activity);
         }
 
-        // DELETE: api/Activities/5
-        public void Delete(int id)
+        // DELETE: api/activities/5
+        [ResponseType(typeof(activity))]
+        public async Task<IHttpActionResult> Deleteactivity(int id)
         {
+            activity activity = await db.activities.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            db.activities.Remove(activity);
+            await db.SaveChangesAsync();
+
+            return Ok(activity);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool activityExists(int id)
+        {
+            return db.activities.Count(e => e.id == id) > 0;
         }
     }
 }
