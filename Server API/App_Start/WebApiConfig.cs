61262456a822de45950139bbf83a386b0423e34d
Tom Postler <tom@postler.me>
2015-03-06 15:48:36 -0600

    41 Surprise

--- Server API/App_Start/WebApiConfig.cs
+++ Server API/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Headers;
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Server_API
@@ -7,9 +9,17 @@ namespace Server_API
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-            
+            // Make JSON the default response type
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            // April Fools 2015
+            if (DateTime.Today.Date.Equals(new DateTime(2015, 4, 1).Date))
+            {
+                config.Formatters.Clear();
+                config.Formatters.Add(new Formatters.DsonFormatter());
+            }
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
