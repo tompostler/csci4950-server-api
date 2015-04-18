09bc4fb9f37f85c708597da371a9eaddbcde0ce3
Tom Postler <tom@postler.me>
2015-04-18 14:57:07 -0500

    Updated DB [re]generation SQL file (fixed #47)

--- Server API/sql/dbcreate.sql
+++ Server API/sql/dbcreate.sql
@@ -1,112 +1,187 @@
-
-CREATE TABLE [users] (
-  [id] int NOT NULL IDENTITY (1, 1) ,
-  [fname] nvarchar(50) NOT NULL ,
-  [lname] nvarchar(50) NOT NULL ,
-  [email] nvarchar(50) NOT NULL ,
-  [password] char(128) NOT NULL  -- SHA512, 
- PRIMARY KEY ([id])
-) ON [PRIMARY]
+USE [csci4950s15]
 GO
 
-CREATE TABLE [activities] (
-  [id] int NOT NULL IDENTITY (1, 1) ,
-  [user_id] int NOT NULL ,
-  [course_id] varchar(12) NOT NULL ,
-  [name] nvarchar(50) NOT NULL ,
-  [description] nvarchar(100) ,
-  [ddate] datetime ,
-  [mdate] datetime NOT NULL , 
- PRIMARY KEY ([id])
-) ON [PRIMARY]
+/****** Drop all the constraints ******/
+ALTER TABLE [dbo].[tags_activityunits] DROP CONSTRAINT [FK__tags_acti__tag_i__2EA5EC27]
+GO
+ALTER TABLE [dbo].[tags_activityunits] DROP CONSTRAINT [FK__tags_acti__activ__2DB1C7EE]
+GO
+ALTER TABLE [dbo].[tags_activities] DROP CONSTRAINT [FK__tags_acti__tag_i__2F9A1060]
+GO
+ALTER TABLE [dbo].[tags_activities] DROP CONSTRAINT [FK__tags_acti__activ__4589517F]
+GO
+ALTER TABLE [dbo].[settings] DROP CONSTRAINT [FK__settings__user_i__2CBDA3B5]
+GO
+ALTER TABLE [dbo].[locations] DROP CONSTRAINT [FK__locations__user___318258D2]
+GO
+ALTER TABLE [dbo].[auths] DROP CONSTRAINT [FK__auths__user_id__32767D0B]
+GO
+ALTER TABLE [dbo].[activityunits] DROP CONSTRAINT [FK__activityu__locat__3B0BC30C]
+GO
+ALTER TABLE [dbo].[activityunits] DROP CONSTRAINT [FK__activityu__activ__467D75B8]
+GO
+ALTER TABLE [dbo].[activities] DROP CONSTRAINT [FK__activitie__user___44952D46]
+GO
+ALTER TABLE [dbo].[activities] DROP CONSTRAINT [FK__activitie__cours__4D2A7347]
 GO
 
-CREATE TABLE [tags] (
-  [id] tinyint NOT NULL IDENTITY (1, 1) ,
-  [name] nvarchar(50) NOT NULL ,
-  [default_color] char(6) NOT NULL , 
- PRIMARY KEY ([id])
-) ON [PRIMARY]
+/****** Drop all the tables ******/
+DROP TABLE [dbo].[users]
+GO
+DROP TABLE [dbo].[tags_activityunits]
+GO
+DROP TABLE [dbo].[tags_activities]
+GO
+DROP TABLE [dbo].[tags]
+GO
+DROP TABLE [dbo].[settings]
+GO
+DROP TABLE [dbo].[locations]
+GO
+DROP TABLE [dbo].[courses]
+GO
+DROP TABLE [dbo].[auths]
+GO
+DROP TABLE [dbo].[activityunits]
+GO
+DROP TABLE [dbo].[activities]
 GO
 
-CREATE TABLE [tags_activities] (
-  [tag_id] tinyint NOT NULL ,
-  [activity_id] int NOT NULL , 
+/****** Create all the tables, alphabetically ******/
+CREATE TABLE [dbo].[activities](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[course_id] [varchar](12) NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[description] [nvarchar](100) NULL,
+	[ddate] [datetime2](3) NULL,
+	[eduration] [real] NULL,
+	[pduration] [real] NULL,
+	[importance] [tinyint] NULL,
+	[mdate] [datetime2](3) NOT NULL DEFAULT (getutcdate()),
+ PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [dbo].[activityunits](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[activity_id] [int] NOT NULL,
+	[location_id] [int] NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[description] [nvarchar](100) NULL,
+	[stime] [datetime2](3) NOT NULL,
+	[etime] [datetime2](3) NOT NULL,
+	[mdate] [datetime2](3) NOT NULL DEFAULT (getutcdate()),
+ PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [dbo].[auths](
+	[user_id] [int] NOT NULL,
+	[token] [char](64) NOT NULL,
+	[expire] [datetime2](3) NOT NULL,
+ PRIMARY KEY ([user_id])
+)
+GO
+
+CREATE TABLE [dbo].[courses](
+	[id] [varchar](12) NOT NULL,
+	[name] [varchar](100) NOT NULL,
+ PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [dbo].[locations](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[name] [nvarchar](50) NULL,
+	[content] [nvarchar](100) NOT NULL,
+	[mdate] [datetime2](3) NOT NULL DEFAULT (getutcdate()),
+ PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [dbo].[settings](
+	[user_id] [int] NOT NULL,
+	[value] [ntext] NOT NULL,
+	[mdate] [datetime2](3) NOT NULL DEFAULT (getutcdate()),
+ PRIMARY KEY ([user_id])
+)
+GO
+
+CREATE TABLE [dbo].[tags](
+	[id] [tinyint] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[default_color] [char](6) NOT NULL,
+ PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [dbo].[tags_activities](
+	[tag_id] [tinyint] NOT NULL,
+	[activity_id] [int] NOT NULL,
  PRIMARY KEY ([tag_id], [activity_id])
-) ON [PRIMARY]
+)
 GO
 
-CREATE TABLE [locations] (
-  [id] int NOT NULL IDENTITY (1, 1) ,
-  [user_id] int NOT NULL ,
-  [name] nvarchar(50) ,
-  [content] nvarchar(100) NOT NULL , 
- PRIMARY KEY ([id])
-) ON [PRIMARY]
-GO
-
-CREATE TABLE [activityunits] (
-  [id] bigint NOT NULL IDENTITY (1, 1) ,
-  [activity_id] int NOT NULL ,
-  [location_id] int NOT NULL ,
-  [name] nvarchar(50) NOT NULL ,
-  [description] nvarchar(100) ,
-  [stime] datetime NOT NULL ,
-  [etime] datetime NOT NULL , 
- PRIMARY KEY ([id])
-) ON [PRIMARY]
-GO
-
-CREATE TABLE [tags_activityunits] (
-  [tag_id] tinyint NOT NULL ,
-  [activityunit_id] bigint NOT NULL , 
+CREATE TABLE [dbo].[tags_activityunits](
+	[tag_id] [tinyint] NOT NULL,
+	[activityunit_id] [bigint] NOT NULL,
  PRIMARY KEY ([tag_id], [activityunit_id])
-) ON [PRIMARY]
+)
 GO
 
-CREATE TABLE [auths] (
-  [user_id] int NOT NULL ,
-  [token] char(64) NOT NULL , -- SHA 256
-  [expire] datetime NOT NULL  -- Tokens last 24hrs, 
- PRIMARY KEY ([user_id])
-) ON [PRIMARY]
+CREATE TABLE [dbo].[users](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[fname] [nvarchar](50) NOT NULL,
+	[lname] [nvarchar](50) NOT NULL,
+	[email] [nvarchar](50) NOT NULL,
+	[password] [char](60) NOT NULL,
+	[mdate] [datetime2](3) NOT NULL DEFAULT (getutcdate()),
+ PRIMARY KEY ([id]),
+ CONSTRAINT [AK_email] UNIQUE ([email])
+)
 GO
 
-CREATE TABLE [settings] (
-  [user_id] int NOT NULL ,
-  [value] ntext NOT NULL ,
-  [mdate] datetime NOT NULL , 
- PRIMARY KEY ([user_id])
-) ON [PRIMARY]
+/****** Add all the constraints ******/
+ALTER TABLE [dbo].[activities] ADD FOREIGN KEY([course_id]) REFERENCES [dbo].[courses] ([id])
 GO
 
-CREATE TABLE [courses] (
-  [id] varchar(12) NOT NULL ,
-  [name] nvarchar(32) NOT NULL , 
- PRIMARY KEY ([id])
-) ON [PRIMARY]
+ALTER TABLE [dbo].[activities] ADD FOREIGN KEY([user_id]) REFERENCES [dbo].[users] ([id])
+ON DELETE CASCADE
 GO
 
-ALTER TABLE [activities] ADD FOREIGN KEY (user_id) REFERENCES [users] ([id]);
-				
-ALTER TABLE [activities] ADD FOREIGN KEY (course_id) REFERENCES [courses] ([id]);
-				
-ALTER TABLE [tags_activities] ADD FOREIGN KEY (tag_id) REFERENCES [tags] ([id]);
-				
-ALTER TABLE [tags_activities] ADD FOREIGN KEY (activity_id) REFERENCES [activities] ([id]);
-				
-ALTER TABLE [locations] ADD FOREIGN KEY (user_id) REFERENCES [users] ([id]);
-				
-ALTER TABLE [activityunits] ADD FOREIGN KEY (activity_id) REFERENCES [activities] ([id]);
-				
-ALTER TABLE [activityunits] ADD FOREIGN KEY (location_id) REFERENCES [locations] ([id]);
-				
-ALTER TABLE [tags_activityunits] ADD FOREIGN KEY (tag_id) REFERENCES [tags] ([id]);
-				
-ALTER TABLE [tags_activityunits] ADD FOREIGN KEY (activityunit_id) REFERENCES [activityunits] ([id]);
-				
-ALTER TABLE [auths] ADD FOREIGN KEY (user_id) REFERENCES [users] ([id]);
-				
-ALTER TABLE [settings] ADD FOREIGN KEY (user_id) REFERENCES [users] ([id]);
-				
-ALTER TABLE [auths] ADD CONSTRAINT DF_auths DEFAULT DATEADD(day, 1, GETDATE()) FOR [expire];
+ALTER TABLE [dbo].[activityunits] ADD FOREIGN KEY([activity_id]) REFERENCES [dbo].[activities] ([id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[activityunits] ADD FOREIGN KEY([location_id]) REFERENCES [dbo].[locations] ([id])
+GO
+
+ALTER TABLE [dbo].[auths] ADD FOREIGN KEY([user_id]) REFERENCES [dbo].[users] ([id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[locations] ADD FOREIGN KEY([user_id]) REFERENCES [dbo].[users] ([id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[settings] ADD FOREIGN KEY([user_id]) REFERENCES [dbo].[users] ([id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[tags_activities] ADD FOREIGN KEY([activity_id]) REFERENCES [dbo].[activities] ([id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[tags_activities] ADD FOREIGN KEY([tag_id]) REFERENCES [dbo].[tags] ([id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[tags_activityunits] ADD FOREIGN KEY([activityunit_id]) REFERENCES [dbo].[activityunits] ([id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[tags_activityunits] ADD FOREIGN KEY([tag_id]) REFERENCES [dbo].[tags] ([id])
+ON DELETE CASCADE
+GO
