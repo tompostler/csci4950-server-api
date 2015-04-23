2ece3acb4d3b323f5fc1fe9b10141c75e5e4780a
Anthony Nixon <ajn0592@gmail.com>
2015-04-23 00:11:35 -0500

    Stats controller
    Closes #48

--- Server API/Controllers/statsController.cs
+++ Server API/Controllers/statsController.cs
@@ -20,34 +20,17 @@ namespace Server_API.Controllers
 
         public class Stat_API
         {
-            public Stat_API()
-            {
-                tag_ids = new List<byte>();
-                activityunit_ids = new List<long>();
-            }
 
-            [Required, StringLength(12)]
-            public string course_id { get; set; }
+            public float eduration_average { get; set; }
 
-            [Required, StringLength(50)]
-            public string name { get; set; }
+            public float pduration_average { get; set; }
 
-            [StringLength(100)]
-            public string description { get; set; }
+            public float total_eduration { get; set; }
 
-            public DateTime? ddate { get; set; }
+            public float total_pduration { get; set; }
 
-            public float? eduration { get; set; }
+            public int total_activities { get; set; }
 
-            public float? pduration { get; set; }
-
-            public byte? importance { get; set; }
-
-            public DateTime mdate { get; set; }
-
-            public List<byte> tag_ids { get; set; }
-
-            public List<long> activityunit_ids { get; set; }
         }
 
         // GET: api/stats
@@ -66,59 +49,88 @@ namespace Server_API.Controllers
             if (!String.IsNullOrEmpty(course_id))
                 activities = activities.Where(p => p.course_id.Equals(course_id));
 
+            bool includeDate = false;
+            if (ddate.HasValue)
+            {
+                activities = activities.Where(p => p.ddate.HasValue);
+                includeDate = true;
+                //activities = activities.Where(p => ddate.Value.Date.CompareTo(p.ddate.Value.Date) == 0);
+            }
+
             // Convert the activities to more API friendly things
-            List<Stat_API> results = new List<Stat_API>();
             List<activity> activitylist = await activities.ToListAsync();
+
+            float totalPduration = 0;
+            float totalEduration = 0;
+
+            List<float> pdurations = new List<float>();
+            List<float> edurations = new List<float>();
+
+
             foreach (var act in activitylist)
-                results.Add(ConvertActivityToStatApi(act));
+                if (includeDate)
+                {
+                    if (act.ddate.Value.Date.CompareTo(ddate.Value.Date) == 0)
+                    {
+                        if (act.pduration.HasValue)
+                        {
+                            totalPduration += act.pduration.Value;
+                            pdurations.Add(act.pduration.Value);
+                        }
+                        if (act.eduration.HasValue)
+                        {
+                            totalEduration += act.eduration.Value;
+                            edurations.Add(act.eduration.Value);
+                        }
+                    }
+                }
+                else
+                {
+                    if (act.pduration.HasValue)
+                    {
+                        totalPduration += act.pduration.Value;
+                        pdurations.Add(act.pduration.Value);
+                    }
+                    if (act.eduration.HasValue)
+                    {
+                        totalEduration += act.eduration.Value;
+                        edurations.Add(act.eduration.Value);
+                    }
+                }
 
-            if (results.Count == 0)
-                return NotFound();
-            else
-                return Ok(results);
+            int totalActivities = pdurations.Count;
+            float eduration_average = edurations.Average();
+            float pduration_average = pdurations.Average();
+            float total_eduration = edurations.Sum();
+            float total_pduration = pdurations.Sum();
+
+            Stat_API stats = ConvertDataToStatApi(totalActivities, eduration_average, total_eduration, pduration_average, total_pduration);
+
+
+                
+
+           return Ok(stats);
             
             
         }
 
-        // GET: api/stats/5
-        public string Get(int id)
+        // OPTIONS: api/info
+        public IHttpActionResult Options()
         {
-            return "value";
+            return Ok();
         }
 
-        // POST: api/stats
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/stats/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/stats/5
-        public void Delete(int id)
-        {
-        }
-
-        private Stat_API ConvertActivityToStatApi(activity Activity)
+        private Stat_API ConvertDataToStatApi(int total, float eduration_average, float total_eduration, float pduration_average, float total_pduration)
         {
             // Convert the EntityModel type to our API type
-            Stat_API act = new Stat_API();
-            act.course_id = Activity.course_id;
-            act.name = Activity.name;
-            act.description = Activity.description;
-            act.ddate = Activity.ddate;
-            act.eduration = Activity.eduration;
-            act.pduration = Activity.pduration;
-            act.importance = Activity.importance;
-            act.mdate = Activity.mdate;
+            Stat_API stats = new Stat_API();
+            stats.total_activities = total;
+            stats.eduration_average = eduration_average;
+            stats.total_eduration = total_eduration;
+            stats.pduration_average = pduration_average;
+            stats.total_pduration = total_pduration;
 
-            // Magic to get just the IDs out of objects
-            act.tag_ids = Activity.tags.Select(p => p.id).ToList();
-            act.activityunit_ids = Activity.activityunits.Select(p => p.id).ToList();
-
-            return act;
+            return stats;
         }
     }
 }
